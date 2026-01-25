2e35857c6b03165c1292604f4a72aa6cb9f8dc10
ayano-yuki <kd0ayano@gmail.com>
2026-01-25 22:00:06 +0900

    remove: hello wasm

--- src/app/hello-wasm/page.tsx
+++
@@ -1,7 +0,0 @@
-import { FeatureCheckerPage } from "@/page/FeatureCheckerPage";
-import { HelloWasmPage } from "@/page/HelloWasmPage";
-
-const HELLO_WASM = () => {
-  return <HelloWasmPage />;
-}
-export default HELLO_WASM;
